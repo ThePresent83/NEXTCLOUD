4dca2b3989bf2d77963e00a8a629dcbd945a85e7
ThePresent83 <114430067+ThePresent83@users.noreply.github.com>
2024-03-13 11:31:45 +0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocpzakthpdbl',
+  'passwordsalt' => 'bnN2TjDB3ps6bQSpZ9MbWlUsDIQ+H6',
+  'secret' => 'GlOf4//wJESZReaRcpTFoEfb07LU97Pat6XYg0ky0qxf7NCo',
+  'trusted_domains' => 
+  array (
+    0 => '10.100.0.16',
+    1 => 'sanu.ddns.net',
+  ),
+  'datadirectory' => '/var/www/html/nextcloud/data',
+  'dbtype' => 'sqlite3',
+  'version' => '19.0.0.12',
+  'overwrite.cli.url' => 'http://10.100.0.16/nextcloud',
+  'installed' => true,
+);
+ 
